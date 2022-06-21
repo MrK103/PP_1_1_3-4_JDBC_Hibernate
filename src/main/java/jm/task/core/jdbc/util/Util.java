ae57dccee04bf38a7bb4e9ecab91449b56e1f41e
Mark <i.love.rock182@gmail.com>
2022-06-21 15:04:52 +0300

    refactoring Util.java

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -5,17 +5,18 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Util {
-    // реализуйте настройку соеденения с БД
-    public static String URl = "jdbc:mysql://localhost:3306/public";
-    public static String driver = "com.mysql.cj.jdbc.Driver";
-    public static String login = "root";
-    public static String pass = "Success100SS";
-    public static Connection connection;
+    private static Connection connection;
 
     static {
         try {
+            String driver = "com.mysql.cj.jdbc.Driver";
+            String URl = "jdbc:mysql://localhost:3306/public";
+            String login = "root";
+            String pass = "Success100SS";
+
             Class.forName(driver);
             connection = DriverManager.getConnection(URl, login, pass);
+
             System.out.println("Connection succesfull!");
         } catch (SQLException | ClassNotFoundException e) {
             System.err.println("Connection failed");
