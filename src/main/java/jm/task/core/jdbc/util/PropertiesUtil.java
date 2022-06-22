f7f96bd569612334dea3eb569ceb32e07bac35b9
Mark <i.love.rock182@gmail.com>
2022-06-22 11:48:38 +0300

    add properties file and PropertiesUtil.java

---
+++ src/main/java/jm/task/core/jdbc/util/PropertiesUtil.java
@@ -0,0 +1,25 @@
+package jm.task.core.jdbc.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public final class PropertiesUtil {
+
+    private PropertiesUtil() {
+    }
+
+    private static final Properties PROPERTIES = new Properties();
+
+    public static String get(String key) {
+        return PROPERTIES.getProperty(key);
+    }
+
+    static {
+        try (InputStream inputStream = PropertiesUtil.class.getClassLoader().getResourceAsStream("application.properties")) {
+            PROPERTIES.load(inputStream);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -4,27 +4,49 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class Util {
+public final class Util {
     private static Connection connection;
+    private static final String DRIVER_KEY = "db.driver";
+    private static final String URl_KEY = "db.url";
+    private static final String LOGIN_KEY = "db.login";
+    private static final String PASS_KEY = "db.password";
 
     static {
         try {
-            String driver = "com.mysql.cj.jdbc.Driver";
-            String URl = "jdbc:mysql://localhost:3306/public";
-            String login = "root";
-            String pass = "Success100SS";
-
-            Class.forName(driver);
-            connection = DriverManager.getConnection(URl, login, pass);
-
-            System.out.println("Connection succesfull!");
-        } catch (SQLException | ClassNotFoundException e) {
-            System.err.println("Connection failed");
+            Class.forName(PropertiesUtil.get(DRIVER_KEY));
+        } catch (ClassNotFoundException e) {
             e.getStackTrace();
         }
     }
 
-    public Connection getConnection(){
+    private Connection openConnection() {
+        try {
+            connection = DriverManager.getConnection(PropertiesUtil.get(URl_KEY), PropertiesUtil.get(LOGIN_KEY), PropertiesUtil.get(PASS_KEY));
+            System.out.println("Connection was opened");
+            return connection;
+        } catch (SQLException e) {
+            System.out.println("error");
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void closeConnection() {
+        try {
+            if (!connection.isClosed()) connection.close();
+            System.out.println("Connection was closed");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Connection getConnection() {
+        try {
+            if (connection == null || connection.isClosed()) {
+                connection = openConnection();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
         return connection;
     }
 }
